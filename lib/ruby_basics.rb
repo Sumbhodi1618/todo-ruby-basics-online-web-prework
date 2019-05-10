82c8385d437643d482f33b87ee755e856c19b00f
Justin <sumbhodi1618@gmail.com>
2019-05-10 02:22:58 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,7 +6,8 @@ def assign_variable(value = "Bob")
   return value
 end
 
-def argue(argument = "I'm right and you are wrong!")
+def argue("I'm right and you are wrong!, x)
+    x.times do
     puts argument
 end
 end
